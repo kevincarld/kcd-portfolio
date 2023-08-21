a779d599dea90a6a338245b67c182a91f4bded89
Kevin David <kevincarld@gmail.com>
2023-08-21 20:33:11 +1000

    commenced intro

---
+++ src/app/(components)/intro/intro.tsx
@@ -0,0 +1,21 @@
+export default function Intro() {
+  return (
+    <section className="container py-8 md:flex md:justify-between md:py-14">
+      <div>
+        <h1 className="capitalize text-black dark:text-white font-semibold text-3xl">
+          kevin carl david
+        </h1>
+        <h2 className="text-sm">Fullstack Developer</h2>
+      </div>
+
+      <p className="py-8 max-w-md md:pt-0">
+        Adaptable web developer with over 5 years honing skills in custom
+        WordPress. Transitioned to front-end development crafting immersive
+        websites using React, Next.js, Chakra UI and Tailwind CSS. Proficient in
+        headless solutions, including WordPress and Shopify integrations.
+        Currently, I'm actively building a booking platform using Next.js,
+        Strapi, and Tailwind.
+      </p>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Intro from "./(components)/intro/intro";
 
 export default function Home() {
   return (
     <>
-      {/* <Intro /> */}
+      <Intro />
 
       {/* <SelectedWorks /> */}
     </>
